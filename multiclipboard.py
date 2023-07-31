e6dcdb02d43bf288c1a8b908570cb73e02a7b4f5
avinashvyom <avinash.patil@vyomlabs.com>
2023-07-31 15:05:39 +0530

    Formatted the file

--- multiclipboard.py
+++ multiclipboard.py
@@ -4,22 +4,24 @@ import json
 
 SAVED_DATA = "clipboard.json"
 
+
 def save_items(filepath, data):
     with open(filepath, "w") as f:
-        json.dump(data,f,indent=4)
+        json.dump(data, f, indent=4)
+
 
 def load_items(filepath):
-    try:        
+    try:
         with open(filepath, "r") as f:
             data = json.load(f)
             return data
     except:
         return {}
-    
+
 
 if len(sys.argv) == 2:
     command = sys.argv[1]
-    data = load_items(SAVED_DATA)    
+    data = load_items(SAVED_DATA)
     if command == "save":
         key = input("Enter a key: ")
         data[key] = clipboard.paste()
