600841535966cb7898ea7e7e856cfff8995b8839
Prasad Dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2023-08-20 16:01:49 +0530

    ADDED: Code to print values to specific keys in response

--- weather.py
+++ weather.py
@@ -10,7 +10,10 @@ response = requests.get(request_url)
 
 if response.status_code == 200:
     data = response.json()
-    print(json.dumps(data, indent=4, sort_keys=True))
+    weather = data['weather']
+    temperature = data['main']['temp']
+    print(json.dumps(weather, indent=4, sort_keys=True))
+    print(temperature)
 else:
     print("An error occured......")
 
