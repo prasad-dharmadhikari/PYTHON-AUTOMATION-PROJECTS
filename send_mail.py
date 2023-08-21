c2f7f3bf94ec89b30127adfb45bdb7ac6653df31
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2023-08-21 12:36:46 +0530

    ADDED: Code to send mail with plain text

---
+++ send_mail.py
@@ -0,0 +1,23 @@
+import smtplib
+import ssl
+from email.message import EmailMessage
+
+subject = "Email from python"
+body = "This is a test email from python"
+sender = "prasaddharmadhikari265@gmail.com"
+receiver = "prasad.dharmadhikari4669@gmail.com"
+password = input("Enter a password: ")
+
+message = EmailMessage()
+message["From"] = sender
+message["Subject"] = subject
+message["To"] = sender
+message.set_content(body)
+
+context  = ssl.create_default_context()
+print("Sending email..............")
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(sender, password) 
+    server.sendmail(sender,receiver,message.as_string())
+    
+print("Successfully sent...............")
